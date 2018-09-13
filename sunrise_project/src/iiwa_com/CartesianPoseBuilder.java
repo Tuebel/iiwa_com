42c5d93d8d3fb5ff0fa92c6f27d22f8b5561c6b9
Tim Übelhör <TimUebelhoer@live.de>
2018-09-13 09:51:50 +0200

    conversion to meters

--- sunrise_project/src/iiwa_com/CartesianPoseBuilder.java
+++ sunrise_project/src/iiwa_com/CartesianPoseBuilder.java
@@ -8,9 +8,11 @@ public class CartesianPoseBuilder {
 
 	public static CartesianPose buildPose(Frame frame, String baseFrameId,
 			String childFrameId) {
+		// KUKA default units are mm!
 		return CartesianPose.newBuilder().setBaseFrame(baseFrameId)
-				.setChildFrame(childFrameId).setY(frame.getY())
-				.setZ(frame.getZ()).setA(frame.getAlphaRad())
-				.setB(frame.getBetaRad()).setC(frame.getGammaRad()).build();
+				.setChildFrame(childFrameId).setX(frame.getX() / 1000)
+				.setY(frame.getY() / 1000).setZ(frame.getZ() / 1000)
+				.setA(frame.getAlphaRad()).setB(frame.getBetaRad())
+				.setC(frame.getGammaRad()).build();
 	}
 }
